6c8ad9dafddfc5210bb152e7f7155e87150c7169
Pia Diedam <pia.diedam@viadee.de>
2020-05-16 13:24:15 +0200

    Versuch Nr 2 Spring Native-Image

--- parkhaus-spring/parkhaus-manager/src/main/java/de/viadee/parkhaus/manager/ParkhausManagerApp.java
+++ parkhaus-spring/parkhaus-manager/src/main/java/de/viadee/parkhaus/manager/ParkhausManagerApp.java
@@ -3,7 +3,7 @@ package de.viadee.parkhaus.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class ParkhausManagerApp {
    public static void main(String[] args) {
       SpringApplication.run(ParkhausManagerApp.class, args);
